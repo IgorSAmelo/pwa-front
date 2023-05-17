6f0e145e69159382638a3a21ec3f93a0a7659411
IgorSAmelo <103616650+IgorSAmelo@users.noreply.github.com>
2023-05-17 11:18:09 -0300

    Updating frontend

---
+++ index.js
@@ -0,0 +1,24 @@
+function createMessage() {
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    
+
+    console.log(name, email, message)
+    //validaçoes
+
+    fetch('http://127.0.0.1:5000/createmessage', { 
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({name, email, message})
+        })
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((err) => console.log(err));
+}
+
